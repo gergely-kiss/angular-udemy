2530f19a9c1333bab6ccfe5f3c3318a9cfc37ee4
Kiss Gergely <kiss-gergely@outlook.com>
2020-05-04 08:03:27 +0100

    assigment 3.

--- src/app/assigment3/assigment3.component.ts
+++ src/app/assigment3/assigment3.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-assigment3',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Assigment3Component implements OnInit {
 
-  constructor() { }
+  enable = false;
+  clicked = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  doTheTaskOnTheButton() {
+    this.enable ? this.enable = false : this.enable = true;
+    this.clicked.push((this.clicked.length) + ' th click @ ' + new Date());
+  }
 }
